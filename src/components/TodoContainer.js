694fbc2e850cccbac216a06d39e545f90089c640
arturo <ahpareja@gmail.com>
2022-05-30 18:35:11 -0500

    Add todo item

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodosList from './TodoList';
 import Header from './Header';
+import InputTodo from './InputTodo';
 // eslint-disable-next-line react/prefer-stateless-function
 class TodoContainer extends React.Component {
     state = {
@@ -45,10 +46,22 @@ class TodoContainer extends React.Component {
       });
     }
 
+    addTodoItem = (title) => {
+      const newTodo = {
+        id: 4,
+        title,
+        completed: false,
+      };
+      this.setState({
+        todos: [...this.state.todos, newTodo],
+      });
+    };
+
     render() {
       return (
         <div>
           <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
           <TodosList
             todos={this.state.todos}
             handleChangeProps={this.handleChange}

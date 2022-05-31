c4d9ce331421114f62766b2e2eb7327ae9cbae87
arturo <ahpareja@gmail.com>
2022-05-30 19:27:00 -0500

    Fix root element bug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import TodoContainer from './components/TodoContainer';
 
-ReactDOM.render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
     <TodoContainer />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </StrictMode>,
 );
